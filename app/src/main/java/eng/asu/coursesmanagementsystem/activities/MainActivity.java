092ea77909b9ee4f36ee1448ebdeec12f89b1011
Mohamed Sameh <mohamedsameh1998@gmail.com>
2019-07-23 19:09:10 +0200

    Reset MainActivity

--- app/src/main/java/eng/asu/coursesmanagementsystem/activities/MainActivity.java
+++ app/src/main/java/eng/asu/coursesmanagementsystem/activities/MainActivity.java
@@ -3,17 +3,8 @@ package eng.asu.coursesmanagementsystem.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 
 import eng.asu.coursesmanagementsystem.R;
-import eng.asu.coursesmanagementsystem.interfaces.ClassAsyncGetInterface;
-import eng.asu.coursesmanagementsystem.model.Course;
-import eng.asu.coursesmanagementsystem.services.CoursesAsyncGet;
-import eng.asu.coursesmanagementsystem.services.GetRequestService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,20 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CoursesAsyncGet runner = new CoursesAsyncGet();
-        ClassAsyncGetInterface[] params ;
-        runner.execute(new ClassAsyncGetInterface(){
-            @Override
-            public String[] getParams() {
-                return new String[0];
-            }
 
-            @Override
-            public void postExecution(Course[] courses) {
-                for(Course c: courses)
-                    System.out.println(c.getName());
-            }
-        });
     }
 
 }
