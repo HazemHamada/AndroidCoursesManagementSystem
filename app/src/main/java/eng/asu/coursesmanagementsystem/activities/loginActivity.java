3f1190281c26fb0fb3fbf5d0000494e02d189093
Nagy <nagyraouf.nr@gmail.com>
2019-07-23 07:40:00 +0200

    login ui

---
+++ app/src/main/java/eng/asu/coursesmanagementsystem/activities/loginActivity.java
@@ -0,0 +1,30 @@
+package eng.asu.coursesmanagementsystem.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import eng.asu.coursesmanagementsystem.R;
+
+public class loginActivity extends AppCompatActivity {
+    private EditText email , password;
+    private Button btn_login;
+    private TextView register;
+    private ProgressBar loading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        btn_login=findViewById(R.id.btn_login);
+        loading=findViewById(R.id.loading);
+        register=findViewById(R.id.register);
+    }
+}
